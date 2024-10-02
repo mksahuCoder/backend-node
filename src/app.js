134f082ff443c321e777c47f8e9739a1f623b172
Mithilesh Kumar <mksahu.engineer@gmail.com>
2024-10-02 08:53:48 +0530

    controller routes middleware cloudinary

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 const limit = "16kb";
-app.use(express.json({limit:limit}));
-app.use(express.urlencoded({extended:true, limit:limit}));
+app.use(express.json({ limit: limit }));
+app.use(express.urlencoded({ extended: true, limit: limit }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRoutes from "./routes/user.routes.js";
+// import router from "./routes/user.routes";
+
+// routes declaration
+app.use("/api/v1/users", userRoutes);
 
 export { app };
