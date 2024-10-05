afec0d176095598557e37006ad41931b5f5df2fc
Mithilesh Kumar <mksahu.engineer@gmail.com>
2024-10-05 22:15:56 +0530

    register

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,6 +16,7 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath)
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
